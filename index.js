c3c6b64caf8f8dc9e860e8c7c993489fddd12f0f
Didenko Victoria <155799784+misfits3z@users.noreply.github.com>
2025-06-01 16:36:01 +0800

    index done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { program } from "commander";
+import {listContacts, getContactById, removeContact, addContact} from './src/contacts.js'
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -14,19 +15,23 @@ const options = program.opts();
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const contacts = await listContacts();
+      console.log(contacts);
       break;
 
     case "get":
-      // ... id
+      const contact = await getContactById(id);
+      console.log(contact);
       break;
 
     case "add":
-      // ... name email phone
+      const newContact = await addContact(name, email, phone);
+      console.log(newContact);
       break;
 
     case "remove":
-      // ... id
+      const removedContact = await removeContact(id);
+      console.log(removedContact);
       break;
 
     default:
